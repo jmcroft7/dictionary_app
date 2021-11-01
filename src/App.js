d07c5493f8cb42bfa6efb4a7518efe439504ead6
Johnathan Croft <croftmjohn@gmail.com>
2021-11-01 17:56:36 -0500

    dictionary app test deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Switch, withStyles } from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Definitions from "./components/Definitions";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [category, setCategory] = useState("en");
+  const [LightTheme, setLightTheme] = useState(false);
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
+      );
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+  }, [word, category]);
+
+  const PurpleSwitch = withStyles({
+    switchBase: {
+      color: grey[50],
+      "&$checked": {
+        color: grey[900],
+      },
+      "&$checked + $track": {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        backgroundColor: LightTheme ? "#fff" : "#282c34",
+        color: LightTheme ? "black" : "white",
+        transition: "all 0.5s linear",
+      }}
+    >
+      <Container maxWidth="md" className="container1"
+      >
+        <div className="btn-switch">
+          <div className="group-1">
+            <span>{LightTheme ? "Dark" : "Light"} Mode</span>
+            <PurpleSwitch
+              checked={LightTheme}
+              onChange={() => setLightTheme(!LightTheme)}
+            />
+          </div>
+          <div className="group-2">
+          </div>
+        </div>
+        <Header
+          setWord={setWord}
+          category={category}
+          setCategory={setCategory}
+          word={word}
+          setMeanings={setMeanings}
+          LightTheme={LightTheme}
+        />
+        <Definitions word={word} meanings={meanings} category={category} />
+      </Container>
     </div>
   );
 }
